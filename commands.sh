8938b3e8950ab3184fc8e08009429508c5a8a516
a-robot <86808805+elithaxxor@users.noreply.github.com>
2022-03-17 17:40:15 -0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -9,7 +9,9 @@
 
 #### FOR BROWSER PLUGINS (OSNIT, SELF SECURITY)
 # https://inteltechniques.com/ 
+# ## throw-away email ## 
 
+# tempmailer.de 
 #https://api.wigle.net/
 #https://null-byte.wonderhowto.com/how-to/wardrive-android-phone-map-vulnerable-networks-0176136/
 # grabify.link  ## --> track usersr 
