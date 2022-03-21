5eff49991a49cedf7871c6caefa44cc6876ec2e4
a-robot <86808805+elithaxxor@users.noreply.github.com>
2022-03-21 13:55:11 -0700

    Create nikto_nmap_auto.sh

---
+++ nikto_nmap_auto.sh
@@ -0,0 +1,16 @@
+echo "quick nikto / nmap automation" 
+
+function vuln_scan(){
+	echo $(which bash) 
+	read -p "ip/hostname" ip 
+	read -p "nikto(y/n)" confirm 
+	sudo nmap -sC -sV $ip 
+	if [ "$confirm" == "y"];
+	then 
+		nikto --host $ip 
+	fi 
+}
+
+vuln_scan
+
+
