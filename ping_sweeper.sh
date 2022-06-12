eba44b29d9143bc31584bc3647c662338fe47132
a-robot <86808805+elithaxxor@users.noreply.github.com>
2022-06-12 15:41:30 -0700

    Update ping_sweeper.sh

--- ping_sweeper.sh
+++ ping_sweeper.sh
@@ -4,11 +4,43 @@
 echo 'Enter The Subnet'
 read SUBNET 
 
-function sweep(){
+echo 'Enter Netcat Port' 
+read NC_PORT 
+# NC_PORT=80
+
+
+
+function getInfo() {
+	
+}
+
+function pingSweep(){
 	for ip in {1..254}
 		do ping -c 3 192.168.50.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
 			echo "Update Log: " > sweep_log.txt
 			date >> sweep_log.txt
 	done 
 }
-sweep 
+
+function simpleNetworkScan {
+	netstat
+	ns 
+}
+
+
+function isServerUp {
+if [[ $(nc -z localhost ${NC_PORT}) -eq 0 ]]; then
+	echo "Tomcat is up"
+    echo $(NC_PORT) 
+else
+	echo "Tomcat is shutdown"
+    echo $(NC_PORT) 
+fi
+}
+
+
+simpleNetworkScan
+pingSweep 
+isServerUp
+
+
