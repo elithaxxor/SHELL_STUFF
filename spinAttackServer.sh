c0de81e8719410e5e15dc9f5e39b729187685fb0
a-robot <86808805+elithaxxor@users.noreply.github.com>
2022-06-12 16:36:55 -0700

    Create spinAttackServer.sh

---
+++ spinAttackServer.sh
@@ -0,0 +1,30 @@
+address = "root@45.33.39.233"
+usr = "elithaxxor"
+ipv6 = "2600:3c01::f03c:93ff:fefb:f0f5"
+expoits = "https://github.com/elithaxxor/SHELL_STUFF"
+hashing = "https://github.com/elithaxxor/shell_hashStuff"
+express = "https://86qnr9fs.r.us-east-1.awstrack.me/L0/https:%2F%2Fwww.exp2links2.net%2Fwelcome%3Fsetup_page_token=eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIn0..i7ol6nd7zNRgRZLC0xrpQQ.qohDKmMKM0l7UfeEsXmTUnKLu2wRsywWPyErRSs7egNyGi29EfaYbfTS1xiGFU4QMKhbb5mPosPcxjA0IbIUkI_gQh8kd06w7pfWPTM5km-gHQMn9Lv_UqGX5YNYC-5VIIZZ78v-8R2an8oEu6i1_Xhva2sEvBzC-OkBMeR3eL1aXz6bF_ibFrIaYg1CvtZV.xfnkhDaIcLWJOeBHvl73y-oNPPN_0EuoI8znd2CAnTo%26utm_campaign=your_setup_link%26utm_content=setup_app_button%26utm_medium=email%26utm_source=customer_email%23linux/1/02000000lkl64vp1-hnmgb318-rdsh-drij-dvlq-49q0ekoa9d80-000000/IvETPh3_jgW28TVoC7VGZCvKMXE=273"
+
+
+function hideMyTracks() {
+proxychains firefox
+proxychains tor 
+proxychains curl ($express) 
+}
+
+function sys_update(){
+	sudo apt-get update && sudo apt-get upgrade -y 
+	## remove unused dependacncies 
+	sudo apt-get autoremove && sudo apt-get autoclean
+	## create log file 
+	echo "Update Log: " > apt_log.txt
+	date >> apt_log.txt
+}
+
+
+function spinAnAttackServer() {
+  proxychains ssh -t elithaxxor@lish-fremont.linode.com ubuntu-us-west
+  }
+  
+  
+
