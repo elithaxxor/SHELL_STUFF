33e3a0bcdb4c6265ce08d26c537f2db080472af8
a-robot <86808805+elithaxxor@users.noreply.github.com>
2022-06-12 18:32:08 -0700

    Update spinAttackServer.sh

--- spinAttackServer.sh
+++ spinAttackServer.sh
@@ -6,6 +6,46 @@ hashing = "https://github.com/elithaxxor/shell_hashStuff"
 express = "https://86qnr9fs.r.us-east-1.awstrack.me/L0/https:%2F%2Fwww.exp2links2.net%2Fwelcome%3Fsetup_page_token=eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIn0..i7ol6nd7zNRgRZLC0xrpQQ.qohDKmMKM0l7UfeEsXmTUnKLu2wRsywWPyErRSs7egNyGi29EfaYbfTS1xiGFU4QMKhbb5mPosPcxjA0IbIUkI_gQh8kd06w7pfWPTM5km-gHQMn9Lv_UqGX5YNYC-5VIIZZ78v-8R2an8oEu6i1_Xhva2sEvBzC-OkBMeR3eL1aXz6bF_ibFrIaYg1CvtZV.xfnkhDaIcLWJOeBHvl73y-oNPPN_0EuoI8znd2CAnTo%26utm_campaign=your_setup_link%26utm_content=setup_app_button%26utm_medium=email%26utm_source=customer_email%23linux/1/02000000lkl64vp1-hnmgb318-rdsh-drij-dvlq-49q0ekoa9d80-000000/IvETPh3_jgW28TVoC7VGZCvKMXE=273"
 
 
+
+
+function sys_update(){
+	sudo apt-get update && sudo apt-get upgrade -y 
+	## remove unused dependacncies 
+	sudo apt-get autoremove && sudo apt-get autoclean
+	## create log file 
+	echo "Update Log: " > apt_log.txt
+	date >> apt_log.txt
+}
+
+
+
+function spinAnAttackServer() {
+mkdir ~/home/EXPLOITS/
+	iptables -t mangle -I POSTROUTING 1 -j TTL --ttl-set 66
+	  proxychains ssh -t elithaxxor@lish-fremont.linode.com ubuntu-us-west
+	  Git clone https://github.com/elithaxxor/shell_hashStuff
+	git clone https://github.com/elithaxxor/SHELL_STUFF/tree/main_pi
+	git clone $exploits
+	git clone $hashing 
+	
+  }
+  
+  function getSysInfo() {
+	apt install infix 
+	sudo apt-get install ufw
+	sudo ufw status > LinsUfwStatus.txt
+	sudo service ssh status
+	infix -FXZ > LinsInfix 
+}
+
+
+function setupExpress {
+sudo mkdir ~/home/EXPRESS/ && cd ~/home/EXPRESS/
+curl $express && chmod +777 -R ./
+
+ }
+ 
+
 function hideMyTracks() {
 proxychains firefox
 proxychains tor 
@@ -19,8 +59,7 @@ iptables -t mangle -I POSTROUTING 1 -j TTL --ttl-set 66
 function buildFakeAP () {
 
 
-## make fake dns 
-
+## make fake dns =
 # https://cybergibbons.com/security-2/quick-and-easy-fake-wifi-access-point-in-kali/
 
 sudo apt-get install dnsmasq ## to create multiple dns points
@@ -47,16 +86,10 @@ hostapd ./hostapd.conf
 }
 
 function runFakeAPRouting () {
-sudo sysctl -w net.ipv4.ip_forward=1
-sudo iptables -P FORWARD ACCEPT
-sudo iptables --table nat -A POSTROUTING -o wlan0 -j MASQUERADE
+	sudo sysctl -w net.ipv4.ip_forward=1
+	sudo iptables -P FORWARD ACCEPT
+	sudo iptables --table nat -A POSTROUTING -o wlan0 -j MASQUERADE
 }
-
-
-
-
-
-
 
 
 # https://fedingo.com/how-to-install-openssl-in-ubuntu/
@@ -110,35 +143,6 @@ function makesslKeys() {
 	openssl dgst -sha256 -sign macair.key -out signer verifcation.enc
 	# to sign 
 	openssl base64 -in signer -out verifcation.enc 
-}
-
-function sys_update(){
-	sudo apt-get update && sudo apt-get upgrade -y 
-	## remove unused dependacncies 
-	sudo apt-get autoremove && sudo apt-get autoclean
-	## create log file 
-	echo "Update Log: " > apt_log.txt
-	date >> apt_log.txt
-}
-
-
-
-function spinAnAttackServer() {
-mkdir ~/home/EXPLOITS/
-
-	  proxychains ssh -t elithaxxor@lish-fremont.linode.com ubuntu-us-west
-	  Git clone https://github.com/elithaxxor/shell_hashStuff
-	git clone https://github.com/elithaxxor/SHELL_STUFF/tree/main_pi
-	git clone 
-  }
-  
-  function getSysInfo() {
-	apt install infix 
-	sudo apt-get install ufw
-	sudo ufw status
-	iptables -t mangle -I POSTROUTING 1 -j TTL --ttl-set 66
-	sudo service ssh status
-	infix -FXZ
 }
 
 
